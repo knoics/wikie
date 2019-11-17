3eed1bab74b4dc490e3bbd37f1bc0a1a714f9cd3
Ligang Wang <ligangwangs@gmail.com>
2019-11-17 18:48:47 -0500

    generate number to words mapping

--- wiki_to_number.py
+++ wiki_to_number.py
@@ -1,4 +1,5 @@
 import time
+from collections import defaultdict
 
 from wikie import parse_word_and_pronunciations, pronunciation_to_numbers
 
@@ -13,8 +14,42 @@ def wiktionary_to_number(file_path, max_count=None):
         if max_count and count > max_count:
             break
 
-start_time = time.time()
-with open("./word_numbers.txt", "w") as text_file:
-    for count, word, pronunciation, numbers in wiktionary_to_number(r'./enwiktionary-latest-pages-articles.xml.bz2'):
-        text_file.write("%s|%s|%s|%s\n" % (count, word, pronunciation, numbers))
-print('done!, took %s seconds.' % (time.time() - start_time))
+def extract_word_numbers(wiktionary_file, word_number_file):
+    start_time = time.time()
+    with open(word_number_file, "w") as text_file:
+        for count, word, pronunciation, numbers in wiktionary_to_number(wiktionary_file):
+            text_file.write("%s|%s|%s|%s\n" % (count, word, pronunciation, numbers))
+    print('done!, took %s seconds.' % (time.time() - start_time))
+
+def convert_number_to_words(word_number_file):
+    numbers = defaultdict(list)
+    with open(word_number_file, "r") as text_file:
+        while True:
+            line = text_file.readline()
+            if not line:
+                break
+            items = line.split('|')
+            word = items[1]
+            number = items[3].strip()
+            if not number:
+                continue
+            numbers[number].append(word)
+    return numbers   
+
+def write_number_to_words(numbers, number_word_file):
+    with open(number_word_file, "w") as text_file:
+        for number, words in numbers.items():
+            text_file.write("%s||%s\n" % (number, '|'.join(words)))
+    
+#extract_word_numbers(r'./enwiktionary-latest-pages-articles.xml.bz2', r'./word_numbers.txt')
+write_number_to_words(convert_number_to_words(r'./word_numbers.txt'), r'./number_words.txt')
+
+with open(r'./number_words.txt', "r") as text_file:
+    while True:
+        line = text_file.readline()
+        if not line:
+            break
+        number_words = line.split('||')
+        assert len(number_words) == 2, 'format not correct ! %s' % line
+
+print('verified')
