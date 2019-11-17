4f7a0375c59c99e87b5a39e81e1a2174ce1f2fbb
Ligang Wang <ligangwangs@gmail.com>
2019-11-17 08:59:28 -0500

    change to use | separator

--- wiki_to_number.py
+++ wiki_to_number.py
@@ -1,3 +1,5 @@
+import time
+
 from wikie import parse_word_and_pronunciations, pronunciation_to_numbers
 
 def wiktionary_to_number(file_path, max_count=None):
@@ -8,7 +10,11 @@ def wiktionary_to_number(file_path, max_count=None):
         count += 1
         if max_count and count > max_count:
             break
-
+        
+start_time = time.time()
 with open("./word_numbers.txt", "w") as text_file:
-    for count, word, pronunciation, numbers in wiktionary_to_number(r'./enwiktionary-latest-pages-articles.xml.bz2', 1000):
-        text_file.write("%s,%s,%s,%s\n" % (count, word, pronunciation, numbers))
+    for count, word, pronunciation, numbers in wiktionary_to_number(r'./enwiktionary-latest-pages-articles.xml.bz2'):
+        if ',' not in word and ',' in pronunciation:
+            pronunciation = pronunciation.split(',')[0]
+        text_file.write("%s|%s|%s|%s\n" % (count, word, pronunciation, numbers))
+print('done!, took %s seconds.' % (time.time() - start_time))
